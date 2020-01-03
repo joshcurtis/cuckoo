fd8f06ad09df498a4c94b190f6aecd1a0a3f5faf
Josh Curtis <jcurtis825@gmail.com>
2020-01-02 17:37:58 -0800

    Implement a basic cuckoo set

--- src/main/java/CuckooSet.java
+++ src/main/java/CuckooSet.java
@@ -1,73 +1,107 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 import java.util.function.IntFunction;
 
 import org.apache.commons.codec.digest.MurmurHash3;
 
 public class CuckooSet<T> {
-  public static final int BUCKET_SIZE = 1;
-  T[][] slots;
-  // TODO how to represent int -> long?
+  private T[][] tables;
   List<IntFunction<Long>> hashes;
-  private final static int MAX_COLLISIONS = 50;
 
   public CuckooSet(int capacity) {
-    //noinspection unchecked
-    slots = (T[][])new Object[capacity][BUCKET_SIZE];
     hashes = new ArrayList<>();
     hashes.add(MurmurHash3::hash64);
-    hashes.add(hashCode -> (long)hashCode);
+    hashes.add(i -> Long.valueOf(i));
+    tables = (T[][])new Object[hashes.size()][capacity];
   }
-
   boolean insert(T object) {
-    int hc = object.hashCode();
-    for (IntFunction<Long> hash : hashes) {
-      int idx = project(hash.apply(hc), slots.length);
-      T[] bucket = slots[idx];
-      for (int i = 0; i < BUCKET_SIZE; i++) {
-        // TODO what if it's already here
-        if (bucket[i] == null) {
-          bucket[i] = object;
+    if (contains(object)) {
+      return false;
+    } else {
+      T res = insertElement(object, tables);
+      if (res == null) {
+        return true;
+      } else {
+        tables = rehash(tables);
+        return insert(res);
+      }
+    }
+  }
+
+  private T[][] rehash(T[][] tables) {
+    T[][] tmp = (T[][])new Object[tables.length][2 * tables[0].length];
+    for (T[] table : tables) {
+      // TODO use buckets
+      for (T elem : table) {
+        if (elem != null) {
+          T res = insertElement(elem, tmp);
+          assert res == null;
         }
       }
     }
-    // TODO resolve collisions
+    return tmp;
   }
 
-  boolean contains(T object) {
-    int hc = object.hashCode();
-    for (IntFunction<Long> hash : hashes) {
-      int idx = project(hash.apply(hc), slots.length);
-      T[] bucket = slots[idx];
-      for (int i = 0; i < BUCKET_SIZE; i++) {
-        if (object.equals(bucket[i])) {
-          return true;
+  /*
+    preconditions: none
+    postconditions:
+      - returnVal == null && contains(elem) ||
+        returnVal != null && !contains(returnVal)
+   */
+  private T insertElement(T elem, T[][] tables) {
+    T prev = null;
+    // TODO what's a better way to determine the upper bound on insertions?
+    // We can't make more insertions than the total number of slots and succeed
+    for (int n = 0; n < tables[0].length * tables.length; n++) {
+      for (int t = 0; t < hashes.size(); t++) {
+        int idx = project(hashes.get(t).apply(elem.hashCode()), tables[0].length);
+        // TODO use buckets
+        T[] table = tables[t];
+        prev = table[idx];
+        table[idx] = elem;
+        if (prev == null) {
+          assert contains(elem);
+          return prev;
+        } else {
+          elem = prev;
         }
       }
     }
-    return false;
+    assert !contains(prev);
+    return prev;
   }
 
-  boolean remove(T object) {
-    // TODO dedup remove and contains
-    int hc = object.hashCode();
-    for (IntFunction<Long> hash : hashes) {
-      int idx = project(hash.apply(hc), slots.length);
-      T[] bucket = slots[idx];
-      for (int i = 0; i < BUCKET_SIZE; i++) {
-        if (object.equals(bucket[i])) {
-          bucket[i] = null;
-          return true;
+  public boolean contains(T object) {
+    return find(object, false);
+  }
+
+  public boolean remove(T object) {
+    return find(object, true);
+  }
+
+  private boolean find(T object, boolean delete) {
+    for (int t = 0; t < hashes.size(); t++) {
+      T[] table = tables[t];
+      int idx = project(hashes.get(t).apply(object.hashCode()), table.length);
+      T elem = table[idx];
+      if (object.equals(elem)) {
+        // TODO is there a nicer way to handle deletion?
+        if(delete) {
+          table[idx] = null;
         }
+        return true;
       }
     }
     return false;
   }
 
   // map num between 0 and capacity
+  // TODO: what's this called?
   int project(long hash, int capacity) {
-    // TODO what's the name for this?
     // TODO be fancier
-    return (int)(hash % capacity);
+    int remainder = (int)(hash % capacity);
+    return remainder < 0 ? remainder + capacity : remainder;
   }
 }
--- src/test/java/TestCuckooSet.java
+++ src/test/java/TestCuckooSet.java
@@ -1,5 +1,78 @@
-import static org.junit.Assert.*;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class TestCuckooSet {
+  public static final int INIT_CAPACITY = 64;
+  Random rand = ThreadLocalRandom.current();
+  CuckooSet<Integer> set = new CuckooSet<>(INIT_CAPACITY);
 
+  @Test
+  public void sanityCheckInsert() {
+    for (int i = 0; i < 8 * INIT_CAPACITY + 1; i++) {
+      assertTrue(set.insert(i));
+      assertTrue("does not contain " + i, set.contains(i));
+    }
+    for (int i = 0; i < 8 * INIT_CAPACITY + 1; i++) {
+      assertTrue("set does not contain " + i, set.contains(i));
+      assertTrue("set does not contain " + i, set.contains(i));
+    }
+    for (int i = 8 * INIT_CAPACITY + 1; i < 10000; i++) {
+      assertFalse(set.contains(i));
+    }
+  }
+
+  @Test
+  public void randomInsert() {
+    Set<Integer> inserted = new HashSet<>();
+    for (int i = 0; i < 10000; i++) {
+      int n = rand.nextInt();
+      assertEquals(
+          inserted.add(n),
+          set.insert(n)
+      );
+    }
+    for (Integer n : inserted) {
+      assertTrue(set.contains(n));
+    }
+    for (int i = 0; i < 10000; i++) {
+      int n = rand.nextInt();
+      assertEquals(inserted.contains(n), set.contains(n));
+    }
+  }
+
+  @Test
+  public void sanityCheckRemove() {
+    for (int i = 0; i < 8 * INIT_CAPACITY + 1; i++) {
+      assertTrue(set.insert(i));
+      assertTrue(set.remove(i));
+      assertFalse(set.contains(i));
+    }
+  }
+
+  @Test
+  public void randomRemove() {
+    int bound = 1000000;
+    Set<Integer> inserted = new HashSet<>();
+    for (int i = 0; i < bound / 2; i++) {
+      int n = rand.nextInt(bound);
+      set.insert(n);
+      inserted.add(n);
+    }
+    for (int i = 0; i < bound * 5; i++) {
+      int n = rand.nextInt(bound);
+      assertEquals(
+          inserted.remove(n),
+          set.remove(n)
+      );
+      assertFalse(set.contains(n));
+    }
+  }
 }
