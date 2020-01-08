7aa75bd6c663014c5521c1b4f405e461bde33942
Josh Curtis <jcurtis825@gmail.com>
2020-01-07 19:20:25 -0800

    Store 4 fingerprints per long

--- src/main/java/CuckooFilter.java
+++ src/main/java/CuckooFilter.java
@@ -1,3 +1,4 @@
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.zip.CRC32;
 
 import org.apache.commons.codec.digest.MurmurHash3;
@@ -6,21 +7,36 @@ public class CuckooFilter {
   // each long represents a bucket of size 4
   private final long[] table;
   public static final long FLAG_BIT = 0x8000L;
+  ThreadLocalRandom rand = ThreadLocalRandom.current();
 
   CuckooFilter(int capacity) {
     table = new long[capacity];
   }
 
   boolean insert(long n) {
-    int fp = fingerprint(n);
+    long fp = fingerprint(n);
     int idx = hash(n);
     assert idx == (idx ^ hash(fp) ^ hash(fp));
-    for (int i = 0; i < table.length * 4; i++) {
+    if (contains(n)) {
+      return true;
+    }
+    for (int i = 0; i < 500; i++) {
       assert 0 <= idx && idx < table.length;
       long bucket = table[idx];
-      int prev = (int)bucket & 0xffff;
-      bucket &= ~0xffff;
-      bucket |= fp | FLAG_BIT;
+      int pos;
+      for (pos = 0; pos < 3; pos++) {
+        if ((bucket & (FLAG_BIT << (16 * pos))) == 0) {
+          break;
+        }
+      }
+      if (pos == 4) {
+        pos = rand.nextInt(4);
+      }
+      int shift = pos * 16;
+      long prev = (bucket & 0xffffL << shift) >> shift;
+      bucket &= ~(0xffffL << shift);
+      bucket |= FLAG_BIT << shift;
+      bucket |= fp << shift;
       table[idx] = bucket;
       if ((prev & FLAG_BIT) == 0) {
         return true;
@@ -40,14 +56,19 @@ public class CuckooFilter {
   }
 
   boolean find(long n, boolean delete) {
-    int fp = fingerprint(n);
+    long fp = fingerprint(n);
     int idx = hash(n);
     for (int i = 0; i < 2; i++) {
-      if ((table[idx] & 0x7fff) == fp) {
-        if (delete) {
-          table[idx] &= ~0xffff;
+      long bucket = table[idx];
+      for (int p = 0; p < 4; p++) {
+        int shift = 16 * p;
+        long entry = (bucket & 0xffffL << shift) >> shift;
+        if((entry & FLAG_BIT) != 0 && (entry & 0x7fff) == fp) {
+          if(delete) {
+            table[idx] &= ~(0xffffL << shift);
+          }
+          return true;
         }
-        return true;
       }
       idx ^= hash(fp);
     }
--- src/test/java/TestCuckooFilter.java
+++ src/test/java/TestCuckooFilter.java
@@ -6,25 +6,42 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class TestCuckooFilter {
-  CuckooFilter filter = new CuckooFilter(1024);
+  public static final int CAPACITY = 1024;
+  CuckooFilter filter = new CuckooFilter(CAPACITY);
   ThreadLocalRandom rand = ThreadLocalRandom.current();
 
   @Test
   public void sanityCheckInsert() {
     int n = 0;
     while (filter.insert(n)) {
-      assertTrue(filter.contains(n));
+      for (int i = 0; i <= n; i++) {
+        assertTrue(filter.contains(i));
+      }
       n++;
     }
   }
 
   @Test
   public void sanityCheckRemove() {
-    for (int i = 0; i < 2048; i++) {
+    for (int i = 0; i < 2 * CAPACITY; i++) {
       long n = rand.nextLong();
       assertTrue(filter.insert(n));
       assertTrue(filter.remove(n));
       assertFalse(filter.contains(n));
+    }
+  }
+
+  @Test
+  public void testLoadFactor() {
+    for (int i = 0; i < 10; i++) {
+      filter = new CuckooFilter(CAPACITY);
+      int n =0;
+      while (filter.insert(rand.nextLong())) {
+        n++;
+      }
+      double loadFactor = 100.0 * n / CAPACITY / 4;
+      System.out.println(loadFactor);
+      //assertTrue(loadFactor >= 90);
     }
   }
 }
